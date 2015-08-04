9148eb39a81856053fcecb11a01d5b63d3fc3467
Michael Lewis <frightenedmonkey@gmail.com>
2015-08-03 20:33:24 -0700

    Moving tabs to actual pep8 four spaces stuffs.

--- munger/filemunger.py
+++ munger/filemunger.py
@@ -5,98 +5,96 @@ import csv
 import datamunger
 
 class fileManager(object):
-	def __init__(self):
-		self.home_path = os.environ['HOME']
-		self.backup = self.home_path + "/.munger/hosts_backup.bak"	
-		# Initialize the database object for use by file manager
-		self.dbstuff = datamunger.dataManager()
-		# check for the backup, and create if it doesn't exist. Bad comments.
-		backup_data = self.dbstuff.get_backup_file()
-		if not backup_data:		
-			try:
-				shutil.copy('/etc/hosts', self.home_path + "/.munger/hosts_backup.bak")
-				self.dbstuff.record_backup_data(self.home_path + '/.munger/hosts_backup.bak')
-			except IOError:
-				print "Oops, trouble backing up your hosts file. Check write permissions in the home directory and try running this again."
-				sys.exit(1)
-		else:
-			pass
+    def __init__(self):
+	self.home_path = os.environ['HOME']
+	self.backup = self.home_path + "/.munger/hosts_backup.bak"
+	# Initialize the database object for use by file manager
+	self.dbstuff = datamunger.dataManager()
+	# check for the backup, and create if it doesn't exist. Bad comments.
+	backup_data = self.dbstuff.get_backup_file()
+	if not backup_data:
+	    try:
+	        shutil.copy('/etc/hosts', self.home_path + "/.munger/hosts_backup.bak")
+		self.dbstuff.record_backup_data(self.home_path + '/.munger/hosts_backup.bak')
+            except IOError:
+                print "Oops, trouble backing up your hosts file. Check write permissions in the home directory and try running this again."
+		sys.exit(1)
+	else:
+            pass
 
-	def activate_hosts_set(self, set_to_activate):
-		# Change the active set data
-		if not self.dbstuff.make_set_active(set_to_activate):
-			return False
-		else:
-			temp_file = self._append_sets_to_temp(set_to_activate)
-			if self._copy_to_etc_hosts(temp_file):
-				return True
-			else:
-				return False
+    def activate_hosts_set(self, set_to_activate):
+        # Change the active set data
+        if not self.dbstuff.make_set_active(set_to_activate):
+            return False
+        else:
+	    temp_file = self._append_sets_to_temp(set_to_activate)
+            if self._copy_to_etc_hosts(temp_file):
+	        return True
+	    else:
+	        return False
 
-	def deactivate_hosts(self):
-		"""This method calls a couple other methods to: 
-		1. Create a temp file from backup (let's never touch the backup directly) 
-			(gets a string for the file location backup in the db); 
-		2. Copies over the temp file to /etc/hosts
-		3. Flushes the local DNS cache. 
-		Returns True if all successful
-		Returns False if things fail (most likely because this wasn't run as root."""
-		temp_file = self._create_temp_file_from_backup()
-		if self._copy_to_etc_hosts(temp_file):
-			self.dbstuff.make_all_sets_inactive()
-			return True
-		else:
-			return False
-			
-	def _create_temp_file_from_backup(self):
-		shutil.copy(self.backup, self.home_path + '/.munger/hosts_temp.bak')
-		temp_file = self.home_path + '/.munger/hosts_temp.bak'
-		return temp_file
+    def deactivate_hosts(self):
+        """This method calls a couple other methods to:
+        1. Create a temp file from backup (let's never touch the backup directly)
+            (gets a string for the file location backup in the db);
+        2. Copies over the temp file to /etc/hosts
+        3. Flushes the local DNS cache.
+        Returns True if all successful
+        Returns False if things fail (most likely because this wasn't run as root."""
+        temp_file = self._create_temp_file_from_backup()
+        if self._copy_to_etc_hosts(temp_file):
+            self.dbstuff.make_all_sets_inactive()
+            return True
+        else:
+            return False
 
-	def _append_sets_to_temp(self, set_to_activate):
-		list_of_ips = self.dbstuff.get_all_ip_entries_for_set(set_to_activate)
-		temp_location = self._create_temp_file_from_backup ()
-		with open(temp_location, 'a') as temp_file:
-			for row in list_of_ips:
-				entries = '\t'
-				entries = entries.join(row)
-				temp_file.write(entries + '\n')
-		return temp_location
+    def _create_temp_file_from_backup(self):
+        shutil.copy(self.backup, self.home_path + '/.munger/hosts_temp.bak')
+        temp_file = self.home_path + '/.munger/hosts_temp.bak'
+        return temp_file
 
-	def _copy_to_etc_hosts(self, file_to_copy, deactivate=False):
-		if file_to_copy == None:
-			return False
-		else:
-			try:
-				if _check_platform() != 'Windows'
-					shutil.copy(file_to_copy, '/etc/hosts') # copy tempfile to replace /etc/hosts
-					if _check_platform() == 'Mac':
-						subprocess.call(["dscacheutil", "-flushcache"]) 
-						return True
-					elif _check_platform() == 'Ubuntu':
-						subprocess.call(["/etc/init.d/dns-clean"])
-				else:
-					print "Sorry, no Windows support yet."
-			except IOError:
-				print "You must run this command with sudo; please retry as such. Hint: just type 'sudo !!'."
-				if deactivate == False:
-					self.dbstuff.make_all_sets_inactive()
-				return False
+    def _append_sets_to_temp(self, set_to_activate):
+        list_of_ips = self.dbstuff.get_all_ip_entries_for_set(set_to_activate)
+        temp_location = self._create_temp_file_from_backup ()
+        with open(temp_location, 'a') as temp_file:
+            for row in list_of_ips:
+                entries = '\t'
+	        entries = entries.join(row)
+                temp_file.write(entries + '\n')
+        return temp_location
 
-	def _check_platform(self):
-		the_os = platform.dist()
-		# This looks a bit WTF, but the tuple returned from .dist() on the mac
-		# returns as a tuple of empty strings. But .dist() correctly identifies 
-		# the specific linux distribution e.g., Ubuntu, so gotta use that. 
-		# Stupidness abounds.
-		if the_os[0] == '' and platform.system() == 'Darwin':
-			return 'Mac'
-		elif the_os[0] == 'Ubuntu':
-			return 'Ubuntu'
-		elif the_os[0] == 'Windows':
-			return 'Windows'
-		else:
-			pass
+    def _copy_to_etc_hosts(self, file_to_copy, deactivate=False):
+        if file_to_copy == None:
+            return False
+        else:
+            try:
+                if _check_platform() != 'Windows'
+                    shutil.copy(file_to_copy, '/etc/hosts') # copy tempfile to replace /etc/hosts
+                    if _check_platform() == 'Mac':
+                        subprocess.call(["dscacheutil", "-flushcache"])
+		        return True
+                    elif _check_platform() == 'Ubuntu':
+                        subprocess.call(["/etc/init.d/dns-clean"])
+                    else:
+                        print "Sorry, no Windows support yet."
+            except IOError:
+                print "You must run this command with sudo; please retry as such. Hint: just type 'sudo !!'."
+                if deactivate == False:
+                    self.dbstuff.make_all_sets_inactive()
+                return False
 
-	#def import_csv_file(self, csv_file_path):
-	#	with open(csv_file_path)
+    def _check_platform(self):
+        the_os = platform.dist()
+        # This looks a bit WTF, but the tuple returned from .dist() on the mac
+        # returns as a tuple of empty strings. But .dist() correctly identifies 
+        # the specific linux distribution e.g., Ubuntu, so gotta use that.
+        # Stupidness abounds.
+        if the_os[0] == '' and platform.system() == 'Darwin':
+            return 'Mac'
+        elif the_os[0] == 'Ubuntu':
+            return 'Ubuntu'
+        elif the_os[0] == 'Windows':
+            return 'Windows'
+        else:
+            pass
+
--- munger/tests/datatests.py
+++ munger/tests/datatests.py
@@ -7,139 +7,139 @@ import datamunger
 import unittest
 
 class dataTester (unittest.TestCase):
-	def setUp(self):
-		self.dbstuff = datamunger.dataManager(test=True)
-		self.home_path = os.environ['HOME'] + '/.munger/test.db'
+    def setUp(self):
+        self.dbstuff = datamunger.dataManager(test=True)
+        self.home_path = os.environ['HOME'] + '/.munger/test.db'
 
-	def tearDown(self):
-		self.dbstuff.close_connection()
-		if not os.path.exists(self.home_path):
-			print "All the tests that just ran should not have been successful. You were running without a test.db file. FAIL."
-		else:
-			os.remove(self.home_path)
+    def tearDown(self):
+        self.dbstuff.close_connection()
+        if not os.path.exists(self.home_path):
+            print "All the tests that just ran should not have been successful. You were running without a test.db file. FAIL."
+        else:
+            os.remove(self.home_path)
 
-	def test_does_init_create_test_db(self):
-		self.assertTrue(os.path.exists(self.home_path))
+    def test_does_init_create_test_db(self):
+        self.assertTrue(os.path.exists(self.home_path))
 
-	def test_can_write_and_retrieve_single_ip_entry(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
-		test_results = self.dbstuff.get_all_ip_entries_for_set('this_test_set')
-		test_compare = [['10.10.10.10', 'blammer.com']]
-		self.assertEqual(test_compare, test_results)
+    def test_can_write_and_retrieve_single_ip_entry(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
+        test_results = self.dbstuff.get_all_ip_entries_for_set('this_test_set')
+        test_compare = [['10.10.10.10', 'blammer.com']]
+        self.assertEqual(test_compare, test_results)
 
-	def test_can_get_all_ips_for_set(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.12', 'blammer3.com', 'this_test_set')
-		test_results = self.dbstuff.get_all_ip_entries_for_set('this_test_set')
-		test_compare = [['10.10.10.10', 'blammer.com'],['10.10.10.11', 'blammer2.com'],['10.10.10.12', 'blammer3.com']]
-		self.assertEqual(test_compare, test_results)
+    def test_can_get_all_ips_for_set(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.12', 'blammer3.com', 'this_test_set')
+        test_results = self.dbstuff.get_all_ip_entries_for_set('this_test_set')
+        test_compare = [['10.10.10.10', 'blammer.com'],['10.10.10.11', 'blammer2.com'],['10.10.10.12', 'blammer3.com']]
+        self.assertEqual(test_compare, test_results)
 
-	def test_no_ips_returns_false(self):
-		self.assertFalse(self.dbstuff.get_all_ip_entries_for_set('some_random_text'))
+    def test_no_ips_returns_false(self):
+        self.assertFalse(self.dbstuff.get_all_ip_entries_for_set('some_random_text'))
 
-	def test_no_sets_returns_false(self):
-		self.assertFalse(self.dbstuff.get_all_sets())
+    def test_no_sets_returns_false(self):
+        self.assertFalse(self.dbstuff.get_all_sets())
 
-	def test_all_sets_are_returned(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set2')
-		self.dbstuff.write_single_ip_entry('10.10.10.12', 'blammer3.com', 'this_test_set3')
-		test_results = self.dbstuff.get_all_sets()
-		test_comparison = [['this_test_set'],['this_test_set2'],['this_test_set3']]
-		self.assertEqual(test_results, test_comparison)
+    def test_all_sets_are_returned(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set2')
+        self.dbstuff.write_single_ip_entry('10.10.10.12', 'blammer3.com', 'this_test_set3')
+        test_results = self.dbstuff.get_all_sets()
+        test_comparison = [['this_test_set'],['this_test_set2'],['this_test_set3']]
+        self.assertEqual(test_results, test_comparison)
 
-	def test_no_active_set_returns_false(self):
-		self.assertFalse(self.dbstuff.get_active_set())
+    def test_no_active_set_returns_false(self):
+        self.assertFalse(self.dbstuff.get_active_set())
 
-	def test_already_active_set_returns_false(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
-		self.dbstuff.make_set_active('this_test_set')
-		self.assertFalse(self.dbstuff.make_set_active('this_test_set'))
+    def test_already_active_set_returns_false(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
+        self.dbstuff.make_set_active('this_test_set')
+        self.assertFalse(self.dbstuff.make_set_active('this_test_set'))
 
-	def test_activated_set_is_correctly_returned(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
-		self.dbstuff.make_set_active('this_test_set')
-		result = self.dbstuff.get_active_set()
-		self.assertEqual(result, [['this_test_set']])
+    def test_activated_set_is_correctly_returned(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
+        self.dbstuff.make_set_active('this_test_set')
+        result = self.dbstuff.get_active_set()
+        self.assertEqual(result, [['this_test_set']])
 
-	def test_nonexistent_set_returns_false(self):
-		self.assertFalse(self.dbstuff.make_set_active('oogie-boogie'))
+    def test_nonexistent_set_returns_false(self):
+        self.assertFalse(self.dbstuff.make_set_active('oogie-boogie'))
 
-	def test_correct_active_set_returned_after_multi_activations(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set2')
-		self.dbstuff.write_single_ip_entry('10.10.10.12', 'blammer3.com', 'this_test_set3')
-		self.dbstuff.make_set_active('this_test_set')
-		self.dbstuff.make_set_active('this_test_set2')
-		result = self.dbstuff.get_active_set()
-		self.assertEqual(result, [['this_test_set2']])
+    def test_correct_active_set_returned_after_multi_activations(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set2')
+        self.dbstuff.write_single_ip_entry('10.10.10.12', 'blammer3.com', 'this_test_set3')
+        self.dbstuff.make_set_active('this_test_set')
+        self.dbstuff.make_set_active('this_test_set2')
+        result = self.dbstuff.get_active_set()
+        self.assertEqual(result, [['this_test_set2']])
 
-	def test_all_sets_can_be_made_inactive(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set2')
-		self.dbstuff.write_single_ip_entry('10.10.10.12', 'blammer3.com', 'this_test_set3')
-		self.dbstuff.make_set_active('this_test_set')
-		self.dbstuff.make_all_sets_inactive()
-		self.assertFalse(self.dbstuff.get_active_set())
+    def test_all_sets_can_be_made_inactive(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set2')
+        self.dbstuff.write_single_ip_entry('10.10.10.12', 'blammer3.com', 'this_test_set3')
+        self.dbstuff.make_set_active('this_test_set')
+        self.dbstuff.make_all_sets_inactive()
+        self.assertFalse(self.dbstuff.get_active_set())
 
-	def test_ip_addresses_can_be_updated(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set2')
-		self.dbstuff.update_single_ip_address('blammer.com', '10.10.10.12', 'this_test_set')
-		result = self.dbstuff.get_all_ip_entries_for_set('this_test_set')
-		self.assertEqual(result, [['10.10.10.12', 'blammer.com']])
+    def test_ip_addresses_can_be_updated(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'blammer.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'blammer2.com', 'this_test_set2')
+        self.dbstuff.update_single_ip_address('blammer.com', '10.10.10.12', 'this_test_set')
+        result = self.dbstuff.get_all_ip_entries_for_set('this_test_set')
+        self.assertEqual(result, [['10.10.10.12', 'blammer.com']])
 
-	def test_does_empty_backup_location_return_false(self):
-		self.assertFalse(self.dbstuff.get_backup_file())
+    def test_does_empty_backup_location_return_false(self):
+        self.assertFalse(self.dbstuff.get_backup_file())
 
-	def test_can_backup_location_be_written(self):
-		self.dbstuff.record_backup_data('/jim/jam/bazoo.bak')
-		self.assertEqual(self.dbstuff.get_backup_file(), '/jim/jam/bazoo.bak')
+    def test_can_backup_location_be_written(self):
+        self.dbstuff.record_backup_data('/jim/jam/bazoo.bak')
+        self.assertEqual(self.dbstuff.get_backup_file(), '/jim/jam/bazoo.bak')
 
-	def test_can_backup_location_be_updated(self):
-		self.dbstuff.record_backup_data('/jim/jam/bazoo.bak')
-		self.dbstuff.record_backup_data('/flim/flam/kazoo.bak')
-		self.assertEqual(self.dbstuff.get_backup_file(), '/flim/flam/kazoo.bak')
+    def test_can_backup_location_be_updated(self):
+        self.dbstuff.record_backup_data('/jim/jam/bazoo.bak')
+        self.dbstuff.record_backup_data('/flim/flam/kazoo.bak')
+        self.assertEqual(self.dbstuff.get_backup_file(), '/flim/flam/kazoo.bak')
 
-	def test_can_remove_all_ips_for_a_set(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'dexter.com', 'this_test_set2')
-		self.dbstuff.write_single_ip_entry('10.10.10.13', 'buster.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.14', 'marshall.com', 'this_test_set2')
-		self.dbstuff.delete_all_ips_for_set('this_test_set')
-		self.assertFalse(self.dbstuff.get_all_ip_entries_for_set('this_test_set'))
+    def test_can_remove_all_ips_for_a_set(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'dexter.com', 'this_test_set2')
+        self.dbstuff.write_single_ip_entry('10.10.10.13', 'buster.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.14', 'marshall.com', 'this_test_set2')
+        self.dbstuff.delete_all_ips_for_set('this_test_set')
+        self.assertFalse(self.dbstuff.get_all_ip_entries_for_set('this_test_set'))
 
-	def test_delete_nonexistent_set(self):
-		self.assertFalse(self.dbstuff.delete_all_ips_for_set('blahblahblah'))
+    def test_delete_nonexistent_set(self):
+        self.assertFalse(self.dbstuff.delete_all_ips_for_set('blahblahblah'))
 
-	def test_can_delete_the_active_set(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'dexter.com', 'this_test_set2')
-		self.dbstuff.write_single_ip_entry('10.10.10.13', 'buster.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.14','marshall.com', 'this_test_set2')
-		self.dbstuff.make_set_active('this_test_set')
-		self.dbstuff.delete_all_ips_for_set('this_test_set')
-		self.assertFalse(self.dbstuff.get_active_set())
+    def test_can_delete_the_active_set(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'dexter.com', 'this_test_set2')
+        self.dbstuff.write_single_ip_entry('10.10.10.13', 'buster.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.14','marshall.com', 'this_test_set2')
+        self.dbstuff.make_set_active('this_test_set')
+        self.dbstuff.delete_all_ips_for_set('this_test_set')
+        self.assertFalse(self.dbstuff.get_active_set())
 
-	def test_can_remove_single_ip(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'dexter.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.13', 'buster.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.14','marshall.com', 'this_test_set')
-		self.dbstuff.remove_single_ip_address('buster.com', 'this_test_set')
-		test_results = self.dbstuff.get_all_ip_entries_for_set('this_test_set')
-		self.assertEqual(test_results, [['10.10.10.10', 'hugo.com'],['10.10.10.11', 'dexter.com'],['10.10.10.14','marshall.com']])
+    def test_can_remove_single_ip(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'dexter.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.13', 'buster.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.14','marshall.com', 'this_test_set')
+        self.dbstuff.remove_single_ip_address('buster.com', 'this_test_set')
+        test_results = self.dbstuff.get_all_ip_entries_for_set('this_test_set')
+        self.assertEqual(test_results, [['10.10.10.10', 'hugo.com'],['10.10.10.11', 'dexter.com'],['10.10.10.14','marshall.com']])
 
-	def test_remove_nonexistent_host_returns_false(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
-		self.assertFalse(self.dbstuff.remove_single_ip_address('buster.com', 'this_test_set'))
+    def test_remove_nonexistent_host_returns_false(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
+        self.assertFalse(self.dbstuff.remove_single_ip_address('buster.com', 'this_test_set'))
 
-	def test_no_dupes_for_sets(self):
-		self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
-		self.dbstuff.write_single_ip_entry('10.10.10.11', 'dexter.com', 'this_test_set')
-		test_results = self.dbstuff.get_all_sets()
-		self.assertEqual(len(test_results), 1)
+    def test_no_dupes_for_sets(self):
+        self.dbstuff.write_single_ip_entry('10.10.10.10', 'hugo.com', 'this_test_set')
+        self.dbstuff.write_single_ip_entry('10.10.10.11', 'dexter.com', 'this_test_set')
+        test_results = self.dbstuff.get_all_sets()
+        self.assertEqual(len(test_results), 1)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
